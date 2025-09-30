f95a28829bfef70e68af5f5c2ab7f2936c0dbd1d
saqib9022ii-prog <saqib9022ii@gmail.com>
2025-09-30 22:14:18 +0500

    some change

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,6 +4,7 @@ function Home(){
         <div className="HomeContainer">
             <div className="HomBox">
             <h1>Home Page</h1>
+            <h2>Sqrepo</h2>
             </div>
         </div>
     )
